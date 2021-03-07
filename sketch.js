e9c884cbbeeb5a15916ba5e04fc201dcdf490a6f
christophershin <72586810+christophershin@users.noreply.github.com>
2021-03-07 12:15:48 -0800

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,9 +1,38 @@
+var hours, minutes, seconds;
+
 function setup() {
   createCanvas(800,400);
-  createSprite(400, 200, 50, 50);
+  hr=hour()
+  mn=minute()
+  sc=second()
+  angleMode(DEGREES)
 }
 
 function draw() {
-  background(255,255,255);  
+  background("black");  
   drawSprites();
+  scAngle=map(sc,0,60,0,360)
+  mnAngle=map(mn,0,60,0,360)
+  hrAngle=map(hr,0,12,0,360)
+
+  push()
+  rotate(scAngle)
+  stroke("red")
+  strokeWeight(7)
+  line(50,100,80,0 )
+  pop()
+
+  push()
+  rotate(mnAngle)
+  stroke("green")
+  strokeWeight(7)
+  line(50,100,80,0 )
+  pop()
+
+  push()
+  rotate(hrAngle)
+  stroke("blue")
+  strokeWeight(7)
+  line(60,100,80,0 )
+  pop()
 }
